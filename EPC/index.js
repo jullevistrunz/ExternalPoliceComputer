37be0b41022c1d8e5ef154782c15211eed4719cc
HeyPalu <j.l.strunz@gmail.com>
2023-12-19 01:19:16 +0100

    pre-update version

--- EPC/index.js
+++ EPC/index.js
@@ -2,7 +2,7 @@ const http = require('http')
 const fs = require('fs')
 const url = require('url')
 const os = require('os')
-const version = '1.3.3'
+const version = '1.3.4'
 
 // clear data on start up
 const dataDefaults = new Map([
