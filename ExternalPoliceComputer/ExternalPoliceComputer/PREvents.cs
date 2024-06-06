32746b5003cc19f180e50981d23c193cc2a814b1
jullevistrunz <j.l.strunz@gmail.com>
2024-06-06 18:59:19 +0200

    add new PR events

--- ExternalPoliceComputer/ExternalPoliceComputer/PREvents.cs
+++ ExternalPoliceComputer/ExternalPoliceComputer/PREvents.cs
@@ -2,43 +2,31 @@ using Rage;
 
 namespace ExternalPoliceComputer {
     internal static class PREvents {
-        private static void Events_pedArrestedEvent(Ped ped, Ped officer, bool frontCuffs) {
+        private static void Events_OnPedArrested(Ped ped, Ped officer, bool frontCuffs) {
             DataToClient.AddWorldPed(ped);
         }
 
-        private static void Events_patDownPedEvent(Ped ped) {
+        private static void Events_OnPedPatDown(Ped ped) {
             DataToClient.AddWorldPed(ped);
             DataToClient.UpdateCurrentID(ped);
         }
 
-        private static void Events_stopPedEvent(Ped ped) {
+        private static void Events_OnPedStopped(Ped ped) {
             DataToClient.AddWorldPed(ped);
         }
 
-        // private static void Events_askIdEvent(Ped ped) {
-        //     AddWorldPed(ped);
-        //     UpdateCurrentID(ped);
-        // }
-
-        // private static void Events_askDriverLicenseEvent(Ped ped) {
-        //     AddWorldPed(ped);
-        //     UpdateCurrentID(ped);
-        // }
-        //
-        // private static void Events_askPassengerIdEvent(Vehicle vehicle) {
-        //     Ped[] passengers = vehicle.Passengers;
-        //     for (int i = 0; i < passengers.Length; i++) {
-        //         UpdateCurrentID(passengers[i]);
-        //     }
-        // }
+        private static void Events_OnIdentificationGiven(Ped ped) {
+            DataToClient.AddWorldPed(ped);
+            DataToClient.UpdateCurrentID(ped);
+        }
 
         internal static void SubscribeToPREvents() {
-            PolicingRedefined.API.EventsAPI.OnPedPatDown += Events_patDownPedEvent;
-            PolicingRedefined.API.EventsAPI.OnPedStopped += Events_stopPedEvent;
-            PolicingRedefined.API.EventsAPI.OnPedArrested += Events_pedArrestedEvent;
-            // StopThePed.API.Events.askIdEvent += Events_askIdEvent;
-            // StopThePed.API.Events.askDriverLicenseEvent += Events_askDriverLicenseEvent;
-            // StopThePed.API.Events.askPassengerIdEvent += Events_askPassengerIdEvent;
+            PolicingRedefined.API.EventsAPI.OnPedPatDown += Events_OnPedPatDown;
+            PolicingRedefined.API.EventsAPI.OnPedStopped += Events_OnPedStopped;
+            PolicingRedefined.API.EventsAPI.OnPedArrested += Events_OnPedArrested;
+            PolicingRedefined.API.EventsAPI.OnIdentificationGiven += Events_OnIdentificationGiven;
+            PolicingRedefined.API.EventsAPI.OnDriverIdentificationGiven += Events_OnIdentificationGiven;
+            PolicingRedefined.API.EventsAPI.OnOccupantIdentificationGiven += Events_OnIdentificationGiven;
         }
     }
 }
